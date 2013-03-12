48e9fcf09e40049df87cacd136644bd0945c84ee
Alex Rozanski <anr11@doc.ic.ac.uk>
2013-03-12 10:17:17 +0000

    Add members to struct frame for the frame address and the page that is mapped to the frame

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,4 +1,12 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
+#include <hash.h>
+
+struct frame {
+	struct hash_elem hash_elem;		/* Used to store the frame in the frame table. */
+	int32_t frame_addr;				/* The address of the frame in memory. */
+	struct page *page;				/* Stores the page mapped into this frame */
+};
+
 #endif /* vm/frame.h */
