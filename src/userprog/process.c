63f568fdb1855d235f24f202c1764fdbf15b4c48
Alex Rozanski <alex@rozanski.com>
2013-03-12 16:42:30 +0000

    Call frame_map() on user pages which are allocated using palloc_get_multiple()

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -817,6 +817,8 @@ install_page (void *upage, void *kpage, bool writable)
 {
     struct thread *t = thread_current ();
 
+    
+
     /* Verify that there's not already a page at that virtual
        address, then map our page there. */
     return (pagedir_get_page (t->pagedir, upage) == NULL
