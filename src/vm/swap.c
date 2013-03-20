978f7973b68463a42f5efccf6fe0a0a6e1d618cb
Thomas Rooney <thomas@rooney.co>
2013-03-20 01:36:34 +0000

    initialising swap

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,1 +1,18 @@
-swap.c
+#include "devices/block.h" // For swap block
+#include "threads/synch.h" // For locks
+
+static struct block *swap_block; // Swap Block Pointer
+static size_t swap_size;         // Size of the Swap Block
+static size_t max_pages;         // Number of pages that will fit into the SWAP block
+
+
+void swap_init() {
+	// Get the swap block from the filesystem
+	swap_block = block_get_role (BLOCK_SWAP);
+	if (swap_block == NULL) {
+    	PANIC ("Swap Initialisation Failure: No SWAP Data Partition");
+    }
+    swap_size = block_size (swap_device) * BLOCK_SECTOR_SIZE; // block_size returns the number of sectors in a block
+    max_pages = swap_size / PGSIZE
+}
+
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,2 +1,8 @@
 #ifdef VM_SWAP_H
+
+void swap_init();
+void swap_save(void *page_pointer);
+void swap_load(void *page_pointer);
+void swap_delete(void *page_pointer);
+
 #endif VM_SWAP_H
