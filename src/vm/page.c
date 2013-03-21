31e305c98e8fd46682ba6f639d992c3b003d6db3
Alex Rozanski <anr11@doc.ic.ac.uk>
2013-03-21 11:53:04 +0000

    Clean up the supplemental page table struct pages properly

--- src/vm/page.c
+++ src/vm/page.c
@@ -11,51 +11,6 @@ static struct page *supplemental_get_page_info (struct hash *supplemental_page_t
                                                 void *vaddr);
 static void free_user_page(void* upage);
 
-unsigned
-supplemental_page_table_hash (const struct hash_elem *e, void *aux UNUSED)
-{
-  const struct page *p = hash_entry (e, struct page, hash_elem);
-
-  return hash_bytes (&p->vaddr, sizeof(p->vaddr));
-}
-
-bool
-supplemental_page_table_less (const struct hash_elem *a,
-                const struct hash_elem *b,
-                void *aux UNUSED)
-{
-  const struct page *page_a = hash_entry (a, struct page, hash_elem);
-  const struct page *page_b = hash_entry (b, struct page, hash_elem);
-
-  return page_a->vaddr < page_b->vaddr;
-}
-
-void
-supplemental_page_table_destroy_func (struct hash_elem *e, void *aux UNUSED)
-{
-  struct page *page =  hash_entry (e,
-            struct page,
-            hash_elem);
-
-  switch(page->page_status){
-  case PAGE_FILESYS:
-    free(page->aux);
-    break;
-  case PAGE_SWAP:
-    break;
-  case PAGE_IN_MEMORY:
-    free_user_page(page);
-    break;
-  case PAGE_ZERO:
-    break;
-  case PAGE_MEMORY_MAPPED:
-    break;
-  default:
-    break;
-  }
-
-}
-
 static void
 supplemental_insert_page_info (struct hash *supplemental_page_table,
                                void *vaddr, struct page *page)
@@ -203,4 +158,46 @@ supplemental_is_page_writable (struct hash *supplemental_page_table, void *uaddr
   struct page *page = hash_entry(e, struct page, hash_elem);
 
   return page->writable; 
+}
+
+unsigned
+supplemental_page_table_hash (const struct hash_elem *e, void *aux UNUSED)
+{
+  const struct page *p = hash_entry (e, struct page, hash_elem);
+
+  return hash_bytes (&p->vaddr, sizeof(p->vaddr));
+}
+
+bool
+supplemental_page_table_less (const struct hash_elem *a,
+                const struct hash_elem *b,
+                void *aux UNUSED)
+{
+  const struct page *page_a = hash_entry (a, struct page, hash_elem);
+  const struct page *page_b = hash_entry (b, struct page, hash_elem);
+
+  return page_a->vaddr < page_b->vaddr;
+}
+
+void
+supplemental_page_table_destroy_func (struct hash_elem *e, void *aux UNUSED)
+{
+  struct page *page =  hash_entry (e, struct page, hash_elem);
+
+  switch(page->page_status)
+  {
+    case PAGE_FILESYS:
+      if (page->aux)
+        free(page->aux), page->aux = NULL;
+      break;
+    break;
+    case PAGE_MEMORY_MAPPED:
+      if(page->aux)
+        free (page->aux), page->aux = NULL;
+      break;
+    default:
+      break;
+  }
+
+  free (page);
 }
